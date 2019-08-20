c80cfd58464519fa2efe420ae3245a544cab5279
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-20 18:28:42 -0400

    Modularized Suggestion component

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useMemo } from "react";
 import Card from "./Card";
+import Suggestion from "./Suggestion";
 import HappyHour from "../assets/happyhour.gif";
 import HappyHour2 from "../assets/happyhour1.gif";
 import HappyHour3 from "../assets/happyhour2.gif";
@@ -21,14 +22,6 @@ const hideLoading = css`
   text-align: center;
 `;
 
-const linkStyles = css`
-  text-decoration: underline;
-
-  :hover {
-    cursor: pointer;
-  }
-`;
-
 export default ({ cocktails, updateSearch, searchQuery }) => {
   const image = useMemo(() => {
     const images = [HappyHour, HappyHour2, HappyHour3];
@@ -37,48 +30,18 @@ export default ({ cocktails, updateSearch, searchQuery }) => {
     return images[Math.floor(Math.random() * (max - min)) + min];
   }, []);
 
-  const getIngredient = useCallback(() => {
-    const ingredients = [
-      "rum",
-      "gin",
-      "vodka",
-      "whiskey",
-      "vermouth",
-      "scotch",
-      "mezcal"
-    ];
-    const min = 0;
-    const max = ingredients.length;
-    return ingredients[Math.floor(Math.random() * (max - min)) + min];
-  }, []);
-
-  const ingredient1 = useMemo(() => {
-    const ingredient = getIngredient();
-    return (
-      <span onClick={() => updateSearch(`${ingredient}`)}>
-        {" "}
-        <strong css={linkStyles}>{ingredient}</strong>
-      </span>
-    );
-  }, []);
-  const ingredient2 = useMemo(() => {
-    const ingredient = getIngredient();
-    return (
-      <span onClick={() => updateSearch(`${ingredient}`)}>
-        {" "}
-        <strong css={linkStyles}>{ingredient}</strong>
-      </span>
-    );
-  }, []);
-  const ingredient3 = useMemo(() => {
-    const ingredient = getIngredient();
-    return (
-      <span onClick={() => updateSearch(`${ingredient}`)}>
-        {" "}
-        <strong css={linkStyles}>{ingredient}</strong>
-      </span>
-    );
-  }, []);
+  const memoIngredient1 = useMemo(
+    () => <Suggestion updateSearch={updateSearch} />,
+    []
+  );
+  const memoIngredient2 = useMemo(
+    () => <Suggestion updateSearch={updateSearch} />,
+    []
+  );
+  const memoIngredient3 = useMemo(
+    () => <Suggestion updateSearch={updateSearch} />,
+    []
+  );
 
   return (
     <main css={cocktails.length <= 0 ? hideLoading : showLoading}>
@@ -97,8 +60,8 @@ export default ({ cocktails, updateSearch, searchQuery }) => {
               padding: 15px;
             `}
           >
-            <em>Curious? </em>Try searching for {ingredient1}, {ingredient2}, or{" "}
-            {ingredient3}
+            <em>Curious? </em>Try searching for {memoIngredient1},{" "}
+            {memoIngredient2}, or {memoIngredient3}...
           </h3>
         </>
       ) : null}
---
+++ src/components/Suggestion.js
@@ -0,0 +1,40 @@
+import React, { useCallback, useMemo } from "react";
+
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+const linkStyles = css`
+  text-decoration: underline;
+  font-style: italic;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+export default ({ updateSearch }) => {
+  const getIngredient = useCallback(() => {
+    const ingredients = [
+      "rum",
+      "gin",
+      "vodka",
+      "whiskey",
+      "vermouth",
+      "scotch",
+      "mezcal",
+      "bourbon"
+    ];
+    const min = 0;
+    const max = ingredients.length;
+    return ingredients[Math.floor(Math.random() * (max - min)) + min];
+  }, []);
+
+  const ingredient = getIngredient();
+
+  return (
+    <span onClick={() => updateSearch(`${ingredient}`)}>
+      {" "}
+      <strong css={linkStyles}>{ingredient}</strong>
+    </span>
+  );
+};
