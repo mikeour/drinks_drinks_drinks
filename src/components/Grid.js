5caa03acda8fef4de2fe727d8fd0d0c4de602871
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-20 16:32:26 -0400

    Added HappyHour gif to background

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,22 +1,38 @@
 import React from "react";
 import Card from "./Card";
+import HappyHour from "../happyhour.gif";
 
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
+const showLoading = css``;
+
+const hideLoading = css`
+  grid-area: grid;
+  border: 1px solid black;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 50px;
+  grid-gap: 10px;
+  overflow-y: auto;
+`;
+
 export default ({ cocktails }) => {
   return (
-    <main
-      css={css`
-        grid-area: grid;
-        border: 1px solid black;
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        padding: 50px;
-        grid-gap: 10px;
-        overflow-y: auto;
-      `}
-    >
+    <main css={cocktails ? hideLoading : showLoading}>
+      {cocktails.length <= 0 ? (
+        <img
+          css={css`
+            position: absolute;
+            width: 100px;
+            height: 100px;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+          `}
+          src={HappyHour}
+        />
+      ) : null}
       {cocktails && cocktails.map(props => <Card {...props} />)}
     </main>
   );
