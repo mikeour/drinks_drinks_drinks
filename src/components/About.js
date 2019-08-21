8f55630a4aca89618743aaa94cc1b15ec86bb107
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-21 19:39:02 -0400

    Added additional styling and lorem ipsum text

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,42 @@
 import React from "react";
 
-export default () => <h1>About</h1>;
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+export default () => (
+  <div
+    css={css`
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: center;
+      padding: 100px;
+    `}
+  >
+    <h1>About Drinks, Drinks, Drinks</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ipsa
+      doloribus? Aliquid, recusandae, mollitia ducimus vel atque odio
+      necessitatibus veritatis veniam molestias fugit velit facere quibusdam
+      maiores, quam aspernatur quod.
+    </p>
+    <p>
+      Quos officiis qui neque mollitia ducimus laudantium rerum nostrum
+      perspiciatis, debitis quibusdam obcaecati eligendi doloremque consectetur?
+      Sed deleniti quisquam temporibus voluptatem veniam animi molestiae ipsa
+      officiis, modi, iusto, libero vitae!
+    </p>
+    <p>
+      Exercitationem, veniam asperiores commodi eligendi pariatur ad laudantium
+      inventore incidunt sed iure iste, nobis sequi ut modi ipsa eius! Sapiente
+      ratione dicta perspiciatis ea optio non ex, voluptatem cupiditate
+      veritatis.
+    </p>
+    <p>
+      Corporis provident sunt eligendi optio repellendus doloribus adipisci
+      error fugiat itaque iure voluptatum quia esse explicabo quisquam id, totam
+      commodi hic? Rerum numquam consequatur, neque voluptates nostrum
+      perspiciatis quasi deleniti!
+    </p>
+  </div>
+);
