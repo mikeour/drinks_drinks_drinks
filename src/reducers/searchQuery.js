cc81a249141610316bd965acb9b497e9effcdc83
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-22 20:13:50 -0400

    Added ability to reset search via reducer function

--- src/reducers/searchQuery.js
+++ src/reducers/searchQuery.js
@@ -6,6 +6,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_SEARCH_QUERY":
       return { ...state, searchQuery: action.payload };
+    case "RESET_SEARCH_QUERY":
+      return { ...state, searchQuery: "" };
     default:
       return state;
   }
