03ea7c6c6842b3da836331e502c9a5e956a69710
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-22 20:09:39 -0400

    Moved styles outside of component declaration

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -3,46 +3,34 @@ import React from "react";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
+const burgerStyles = css`
+  width: 19px;
+  height: 3px;
+  background-color: black;
+  margin: 1px 0;
+`;
+
 export default ({ handleSidebar }) => {
   return (
-    <div
-      css={css`
-        display: flex;
-        flex-direction: column;
-        opacity: 0.7;
-        transition: 0.7s linear;
+    <>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          opacity: 0.7;
+          transition: 0.7s linear;
 
-        :hover {
-          opacity: 1;
-          transform: scale(1.1, 1.1);
-        }
-      `}
-      onClick={handleSidebar}
-    >
-      <div
-        css={css`
-          width: 15px;
-          height: 3px;
-          background-color: black;
-          margin: 1px 0;
+          :hover {
+            opacity: 1;
+            transform: scale(1.085, 1.085);
+          }
         `}
-      />
-      <div
-        css={css`
-          width: 15px;
-          height: 3px;
-          background-color: black;
-          margin: 1px 0;
-        `}
-      />
-      <div
-        css={css`
-          width: 15px;
-          height: 3px;
-          background-color: black;
-          margin: 1px 0;
-        `}
-      />
-    </div>
+        onClick={handleSidebar}
+      >
+        <div css={burgerStyles} />
+        <div css={burgerStyles} />
+        <div css={burgerStyles} />
+      </div>
+    </>
   );
 };
