d0fb3a9c521626c1b22f1c80f692e4f6567dc998
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-23 01:49:05 -0400

    Moved styles declaration outside of component

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -3,6 +3,18 @@ import React from "react";
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
+const buttonStyles = css`
+  display: inline-flex;
+  flex-direction: column;
+  opacity: 0.7;
+  transition: 0.7s linear;
+
+  :hover {
+    opacity: 1;
+    transform: scale(1.085, 1.085);
+  }
+`;
+
 const burgerStyles = css`
   width: 19px;
   height: 3px;
@@ -10,27 +22,33 @@ const burgerStyles = css`
   margin: 1px 0;
 `;
 
-export default ({ handleSidebar }) => {
+const Burger = ({ handleSidebar }) => {
   return (
-    <>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          opacity: 0.7;
-          transition: 0.7s linear;
-
-          :hover {
-            opacity: 1;
-            transform: scale(1.085, 1.085);
-          }
-        `}
-        onClick={handleSidebar}
-      >
+    <div
+      css={css`
+        position: relative;
+        display: flex;
+        flex-direction: row;
+      `}
+    >
+      <div css={buttonStyles} onClick={handleSidebar}>
         <div css={burgerStyles} />
         <div css={burgerStyles} />
         <div css={burgerStyles} />
       </div>
-    </>
+      <div
+        css={css`
+          position: absolute;
+          left: 4rem;
+          width: 250px;
+          align-self: center;
+          font-size: 1.4rem;
+        `}
+      >
+        <strong>Drinks, Drinks, Drinks</strong>
+      </div>
+    </div>
   );
 };
+
+export default Burger;
