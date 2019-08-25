cfda4d995a57b76386501cbf7c716abe2aa2294d
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-25 15:19:07 -0400

    Added hook usage and moved custom hook declaration to Parent App

--- src/components/Drink/Drink.js
+++ src/components/Drink/Drink.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link, Redirect } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useCocktailsList, useSearchQuery, useDrinkInfo } from "../App/App";
 
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
@@ -13,27 +13,10 @@ import {
   drinkThumbnailStyles
 } from "./styles";
 
-const useDrinkInfo = () => {
-  const dispatch = useDispatch();
-  const drink = useSelector(state => state.drink.drink);
-  const nextDrink = useSelector(state => state.drink.nextDrink);
-  const prevDrink = useSelector(state => state.drink.prevDrink);
-  const redirect = useSelector(state => state.drink.redirect);
-
-  return {
-    drink,
-    nextDrink,
-    prevDrink,
-    redirect,
-    setDrink: payload => dispatch({ type: "UPDATE_DRINK", payload }),
-    setNextDrink: payload => dispatch({ type: "UPDATE_NEXT_DRINK", payload }),
-    setPrevDrink: payload => dispatch({ type: "UPDATE_PREV_DRINK", payload }),
-    setRedirect: () => dispatch({ type: "UPDATE_REDIRECT" })
-  };
-};
-
-const Drink = ({ location, cocktails, setCocktails, setSearchQuery }) => {
+const Drink = ({ location }) => {
   const drinkID = location.pathname.split("/")[2];
+  const { cocktails, setCocktails } = useCocktailsList();
+  const { setSearchQuery } = useSearchQuery();
   const {
     drink,
     nextDrink,
