aa09f57f1b6751df3ba84e6dfabb5aa0ab24a323
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-25 15:21:44 -0400

    Moved Center Image to child component and refactored grid styles

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,70 +1,84 @@
-import React, { useMemo } from "react";
+import React from "react";
 import Card from "../Card/Card";
+import CenterImage from "../CenterImage/CenterImage";
 import Suggestion from "../Suggestion/Suggestion";
-import HappyHour from "../../assets/happyhour.gif";
-import HappyHour2 from "../../assets/happyhour1.gif";
-import HappyHour3 from "../../assets/happyhour2.gif";
+import { useCocktailsList, useSearchQuery } from "../App/App";
 
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import { showLoading, hideLoading } from "./styles";
 
-const Grid = ({ cocktails, setSearchQuery }) => {
-  const image = useMemo(() => {
-    const images = [HappyHour, HappyHour2, HappyHour3];
-    const min = 0;
-    const max = images.length;
-    return images[Math.floor(Math.random() * (max - min)) + min];
-  }, []);
-
-  const memoIngredient1 = useMemo(
-    () => <Suggestion setSearchQuery={setSearchQuery} />,
-    []
-  );
-  const memoIngredient2 = useMemo(
-    () => <Suggestion setSearchQuery={setSearchQuery} />,
-    []
-  );
-  const memoIngredient3 = useMemo(
-    () => <Suggestion setSearchQuery={setSearchQuery} />,
-    []
-  );
+const Grid = () => {
+  const { cocktails } = useCocktailsList();
+  const { setSearchQuery } = useSearchQuery();
 
   return (
     <main css={cocktails.length <= 0 ? hideLoading : showLoading}>
-      {cocktails.length <= 0 && (
-        <div>
-          <img
+      {cocktails.length <= 0 ? (
+        <CenterImage setSearchQuery={setSearchQuery} />
+      ) : (
+        <>
+          <div
             css={css`
-              width: 250px;
-              height: 250px;
-              opacity: 0.45;
-              margin-top: 100px;
-              margin-left: 10px;
-              animation-delay: 0.5s;
-              animation-duration: 2s;
-              animation-iteration-count: 0.25;
-            `}
-            className="animated shake"
-            src={image}
-          />
-          <h3
-            css={css`
-              padding: 25px;
+              grid-area: left;
+              position: relative;
             `}
           >
-            <em>Need some inspiration? </em>
-            <br />
-            <br />
-            Try searching for {memoIngredient1}, {memoIngredient2}, or maybe
-            {"  "}
-            {memoIngredient3} ...
-          </h3>
-        </div>
-      )}
+            <div
+              css={css`
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+              `}
+            >
+              <p>Other popular searches:</p>
+              <div
+                css={css`
+                  padding: 10px 0;
+                `}
+              >
+                <Suggestion />
+              </div>
+              <div
+                css={css`
+                  padding: 10px 0;
+                `}
+              >
+                <Suggestion />
+              </div>
+              <div
+                css={css`
+                  padding: 10px 0;
+                `}
+              >
+                <Suggestion />
+              </div>
+            </div>
+          </div>
+          <div
+            css={css`
+              grid-area: content;
+              display: grid;
+              grid-template-columns: 1fr;
+              grid-template-rows: repeat(auto-fit, 150px);
+              justify-content: center;
+              grid-gap: 15px;
+              overflow-y: scroll;
 
-      {cocktails &&
-        cocktails.map(props => <Card key={props.idDrink} {...props} />)}
+              padding: 30px 0;
+            `}
+          >
+            {cocktails &&
+              cocktails.map(props => <Card key={props.idDrink} {...props} />)}
+          </div>
+          <div
+            css={css`
+              grid-area: right;
+            `}
+          />
+        </>
+      )}
     </main>
   );
 };
--- src/components/Grid/styles.js
+++ src/components/Grid/styles.js
@@ -1,13 +1,10 @@
 import { css } from "@emotion/core";
 
 export const showLoading = css`
-  grid-area: grid;
   display: grid;
-  grid-template-columns: minmax(240px, 450px);
-  grid-template-rows: repeat(auto-fit, 150px);
-  justify-content: center;
-  grid-gap: 15px;
-  overflow-y: scroll;
+  grid-template-areas: "left content right";
+  grid-template-columns: 3fr 6fr 3fr;
+  overflow: hidden;
 `;
 
 export const hideLoading = css`
