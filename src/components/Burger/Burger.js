23c130ece788a90d51410bbca2ff4cbc88411092
Michael Roeslein <michaeljroeslein@gmail.com>
2019-08-25 15:16:45 -0400

    Added hook usage and logo link to home page

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSidebar, useSearchQuery } from "../App/App";
 
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import { burgerStyles, buttonStyles } from "./styles";
 
-const Burger = ({ handleSidebar }) => {
+const Burger = () => {
+  const { handleSidebar } = useSidebar();
+  const { resetSearchQuery } = useSearchQuery();
+
   return (
     <div
       css={css`
@@ -18,17 +23,25 @@ const Burger = ({ handleSidebar }) => {
         <div css={burgerStyles} />
         <div css={burgerStyles} />
       </div>
-      <div
+      <Link
         css={css`
           position: absolute;
           left: 4rem;
           width: 250px;
           align-self: center;
           font-size: 1.4rem;
+          text-decoration: none;
+          color: black;
+
+          :visited {
+            color: black;
+          }
         `}
+        onClick={resetSearchQuery}
+        to="/"
       >
         <strong>Drinks, Drinks, Drinks</strong>
-      </div>
+      </Link>
     </div>
   );
 };
