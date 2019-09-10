8eff521c3b90b17feba6d864828001c1e9a5f5f2
Michael Roeslein <michaeljroeslein@gmail.com>
2019-09-09 21:32:43 -0400

    Added polyfill for smooth-scrolling browser support

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -7,6 +7,10 @@ import Spinner from "../Spinner/Spinner";
 import { useCocktailsList, useDrinkInfo } from "../../hooks";
 import alphabet from "../../lib/alphabet";
 
+/* Smooth scrolling polyfill for browser-support */
+import smoothscroll from "smoothscroll-polyfill";
+smoothscroll.polyfill();
+
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import {
