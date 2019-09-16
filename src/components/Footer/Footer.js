3f293e5a68514a2af54610cb55155a2c32de0de6
Michael Roeslein <michaeljroeslein@gmail.com>
2019-09-16 19:37:01 -0400

    Added feature to display Users current logged in state in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,44 +1,73 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSearchQuery } from "../../hooks";
 
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
-const Footer = () => (
-  <footer
-    css={css`
-      grid-area: footer;
-      display: flex;
-      justify-content: flex-end;
-      align-items: center;
-      border-top: 1px solid black;
-      background-color: var(--primary);
-    `}
-  >
-    <p
+const Footer = () => {
+  const { resetSearchQuery } = useSearchQuery();
+  return (
+    <footer
       css={css`
-        padding: 0.25rem;
-        font-size: 0.75rem;
+        grid-area: footer;
+        display: flex;
+        justify-content: space-between;
+        padding: 0.5rem 1rem;
+        align-items: center;
+        border-top: 1px solid black;
+        background-color: var(--primary);
       `}
     >
-      ©{" "}
-      <a
-        href="https://www.github.com/mikeour"
-        rel="noopener"
-        target="_blank"
+      <p
         css={css`
-          text-decoration: none;
-          color: black;
-
-          :visited {
-            color: black;
-          }
+          padding: 0.25rem;
+          font-size: 0.75rem;
         `}
       >
-        mikeour
-      </a>
-      , 2019
-    </p>
-  </footer>
-);
+        Currently: Logged Out - Login{" "}
+        <Link
+          css={css`
+            color: black;
+            line-height: 1.7;
+
+            :visited {
+              color: black;
+            }
+          `}
+          to="/login"
+          onClick={resetSearchQuery}
+        >
+          here
+        </Link>
+        .
+      </p>
+      <p
+        css={css`
+          padding: 0.25rem;
+          font-size: 0.75rem;
+        `}
+      >
+        ©{" "}
+        <a
+          href="https://www.github.com/mikeour"
+          rel="noopener"
+          target="_blank"
+          css={css`
+            text-decoration: none;
+            color: black;
+
+            :visited {
+              color: black;
+            }
+          `}
+        >
+          mikeour
+        </a>
+        , 2019
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
