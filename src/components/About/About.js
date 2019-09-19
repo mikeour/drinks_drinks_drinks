8cba17774de786b6ed8e83800ab67bd253033f86
Michael Roeslein <michaeljroeslein@gmail.com>
2019-09-19 16:44:21 -0400

    Added text content to About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,31 +6,18 @@ import { aboutStyles } from "./styles";
 
 const About = () => (
   <div css={aboutStyles} data-testid="about">
-    <h1>About Drinks, Drinks, Drinks</h1>
+    <h1>
+      About <em>Drinks, Drinks, Drinks</em>
+    </h1>
+    <p>My name is Mike and I am a web developer based out of New York.</p>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, ipsa
-      doloribus? Aliquid, recusandae, mollitia ducimus vel atque odio
-      necessitatibus veritatis veniam molestias fugit velit facere quibusdam
-      maiores, quam aspernatur quod.
+      This site exists as an opportunity to combine two of my favorite things:
+      coding and cocktails.
     </p>
     <p>
-      Quos officiis qui neque mollitia ducimus laudantium rerum nostrum
-      perspiciatis, debitis quibusdam obcaecati eligendi doloremque consectetur?
-      Sed deleniti quisquam temporibus voluptatem veniam animi molestiae ipsa
-      officiis, modi, iusto, libero vitae!
+      Next time you're in the mood for a new recipe, I hope you think of this!
     </p>
-    <p>
-      Exercitationem, veniam asperiores commodi eligendi pariatur ad laudantium
-      inventore incidunt sed iure iste, nobis sequi ut modi ipsa eius! Sapiente
-      ratione dicta perspiciatis ea optio non ex, voluptatem cupiditate
-      veritatis.
-    </p>
-    <p>
-      Corporis provident sunt eligendi optio repellendus doloribus adipisci
-      error fugiat itaque iure voluptatum quia esse explicabo quisquam id, totam
-      commodi hic? Rerum numquam consequatur, neque voluptates nostrum
-      perspiciatis quasi deleniti!
-    </p>
+    <p>Stay tuned for new features!</p>
   </div>
 );
 
