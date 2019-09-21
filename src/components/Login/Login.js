2e83fc035c4f77888387f67f326639f350dbd5fd
Michael Roeslein <michaeljroeslein@gmail.com>
2019-09-21 01:26:03 -0400

    Refacted Login component for better styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,18 +6,24 @@ import LoginIcon from "../../assets/login-icon.png";
 
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
+import { hideModalStyles, showModalStyles, formStyles } from "./styles";
 
 const Login = () => {
   const { searchQuery } = useSearchQuery();
   const { isModalShowing, showModal } = useModal();
+  const firstRender = useRef(true);
+
+  // Form state
+  const [formUsername, setFormUsername] = useState("");
   const [formEmail, setFormEmail] = useState("");
   const [formPassword, setFormPassword] = useState("");
-  const firstRender = useRef(false);
 
+  // After Modal closes, reset form value and firstRender ref
   const resetValues = () => {
+    setFormUsername("");
     setFormEmail("");
     setFormPassword("");
-    firstRender.current = false;
+    firstRender.current = true;
   };
 
   const handleChange = (func, e) => {
@@ -25,67 +31,37 @@ const Login = () => {
     func(value);
   };
 
+  // As user inputs into form, trigger a delay followed by pop-up Modal
   useEffect(() => {
-    if (firstRender.current === true) {
-      const delay = setTimeout(() => {
-        showModal();
+    const showModalFrame = () => showModal();
+
+    if (firstRender.current === false) {
+      const delayBeforeModalShows = setTimeout(() => {
+        requestAnimationFrame(showModalFrame);
         resetValues();
       }, 1000);
 
-      return () => clearTimeout(delay);
+      return () => clearTimeout(delayBeforeModalShows);
     }
 
-    firstRender.current = true;
-  }, [formEmail, formPassword]);
+    firstRender.current = false;
+  }, [formUsername, formEmail, formPassword]);
 
+  // If users attempts to search from this page, redirect view to home page
   if (searchQuery.length > 0) {
     return <Redirect to="/"></Redirect>;
   }
+
   return (
     <>
-      <div
-        css={
-          !isModalShowing
-            ? css`
-                display: grid;
-                grid-template-areas:
-                  "info"
-                  "login";
-                grid-template-columns: 1fr;
-                grid-template-rows: 1fr 2fr;
-                overflow-y: auto;
-
-                @media (min-width: 800px) {
-                  grid-template-areas: "info login";
-                  grid-template-columns: 1.25fr 1fr;
-                  grid-template-rows: 1fr;
-                }
-              `
-            : css`
-                display: grid;
-                grid-template-areas: "info login";
-                grid-template-columns: 1.25fr 1fr;
-                grid-template-rows: 1fr;
-                overflow-y: auto;
-                transition: filter 500ms linear;
-                filter: blur(3px);
-
-                @media (max-width: 800px) {
-                  display: flex;
-                  flex-direction: column;
-                  transition: filter 500ms linear;
-                  filter: blur(3px);
-                }
-              `
-        }
-      >
+      <div css={!isModalShowing ? hideModalStyles : showModalStyles}>
         <div
           css={css`
             grid-area: info;
             width: 100%;
             height: 100%;
             position: relative;
-            background: lightblue;
+            background: var(--signup);
           `}
         >
           <div
@@ -158,7 +134,7 @@ const Login = () => {
             height: 100%;
             width: 100%;
             position: relative;
-            background: blanchedalmond;
+            background: var(--alternate);
 
             @media (max-width: 800px) {
               padding: 4rem;
@@ -166,31 +142,9 @@ const Login = () => {
           `}
         >
           <form
-            css={css`
-              display: grid;
-              grid-template-areas:
-                "header"
-                "email"
-                "password"
-                "create";
-              grid-template-columns: 1fr;
-              grid-template-rows: 2fr repeat(3, minmax(60px, 1fr));
-              grid-gap: 2rem;
-              border: 3px solid #555;
-              border-radius: 3%;
-              padding: 2rem 3rem;
-              background: white;
-
-              @media (min-width: 800px) {
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                height: 85%;
-                width: 70%;
-              }
-            `}
+            css={formStyles}
             onClick={e => e.preventDefault()}
+            autoComplete="off"
           >
             <div
               css={css`
@@ -222,20 +176,72 @@ const Login = () => {
             </div>
             <input
               css={css`
-                grid-area: email;
-                font-size: 1.1rem;
+                grid-area: name;
+                font-size: 0.75rem;
                 padding: 0.2rem 0.75rem;
+                font-style: italic;
+              `}
+              type="text"
+              placeholder="Enter Username"
+              value={formUsername}
+              onChange={e => handleChange(setFormUsername, e)}
+            ></input>
+            <div
+              css={css`
+                grid-area: break;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              `}
+            >
+              <span
+                css={css`
+                  padding: 0.5rem;
+                  font-size: 0.85rem;
+                  width: 100%;
+                  display: grid;
+                  align-items: center;
+                  text-align: center;
+                  grid-template-columns: minmax(20px, 1fr) auto minmax(
+                      20px,
+                      1fr
+                    );
+                  grid-gap: 19px;
+                  font-style: italic;
+
+                  ::before {
+                    content: "";
+                    border-top: 1px solid #444;
+                  }
+
+                  ::after {
+                    content: "";
+                    border-top: 1px solid #444;
+                  }
+                `}
+              >
+                or
+              </span>
+            </div>
+            <input
+              css={css`
+                grid-area: email;
+                font-size: 0.75rem;
+                padding: 0.2rem 0.75rem;
+                font-style: italic;
               `}
               type="text"
               placeholder="Enter Email"
+              name="email"
               value={formEmail}
               onChange={e => handleChange(setFormEmail, e)}
             ></input>
             <input
               css={css`
                 grid-area: password;
-                font-size: 1.1rem;
+                font-size: 0.75rem;
                 padding: 0.2rem 0.75rem;
+                font-style: italic;
               `}
               type="password"
               placeholder="Enter Password"
@@ -253,21 +259,46 @@ const Login = () => {
                   background: #6699ff;
                   height: 90%;
                   width: 90%;
+                  transition: background 250ms;
 
                   :hover {
                     cursor: pointer;
+                    background: #3c6bf6;
                   }
                 `}
+                onClick={e => e.preventDefault()}
               >
                 <span
                   css={css`
-                    font-size: 1.1rem;
+                    font-size: 0.75rem;
                     color: white;
                   `}
                 >
                   Submit
                 </span>
               </button>
+            </div>
+            <div
+              css={css`
+                grid-area: forgot;
+                text-align: center;
+              `}
+            >
+              <Link to="/forgot">
+                <em
+                  css={css`
+                    font-size: 0.75rem;
+                    color: blue;
+                    text-decoration: underline;
+
+                    :hover {
+                      cursor: pointer;
+                    }
+                  `}
+                >
+                  Forgot your password?
+                </em>
+              </Link>
             </div>
           </form>
         </div>
---
+++ src/components/Login/styles.js
@@ -0,0 +1,63 @@
+import { css } from "@emotion/core";
+
+export const hideModalStyles = css`
+  display: grid;
+  grid-template-areas:
+    "info"
+    "login";
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 2fr;
+  overflow-y: auto;
+
+  @media (min-width: 800px) {
+    grid-template-areas: "info login";
+    grid-template-columns: 1.25fr 1fr;
+    grid-template-rows: 1fr;
+  }
+`;
+
+export const showModalStyles = css`
+  display: grid;
+  grid-template-areas: "info login";
+  grid-template-columns: 1.25fr 1fr;
+  grid-template-rows: 1fr;
+  overflow-y: auto;
+  transition: filter 500ms linear;
+  filter: blur(3px);
+
+  @media (max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+    transition: filter 500ms linear;
+    filter: blur(3px);
+  }
+`;
+
+export const formStyles = css`
+  display: grid;
+  grid-template-areas:
+    "header"
+    "name"
+    "break"
+    "email"
+    "password"
+    "create"
+    "forgot";
+  grid-template-columns: 1fr;
+  grid-template-rows: 2fr minmax(30px, 1fr) 10px repeat(3, minmax(30px, 1fr)) 25px;
+  grid-gap: 1rem;
+  border: 2px solid #555;
+  border-radius: 4%;
+  padding: 2rem 3rem;
+  background: white;
+  box-shadow: 10px 13px 10px -5px rgba(0, 0, 0, 0.26);
+
+  @media (min-width: 800px) {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 75%;
+    width: 55%;
+  }
+`;
