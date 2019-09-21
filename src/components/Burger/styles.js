671141d28558408aefde62da5cbc6cd46548f3e8
Michael Roeslein <michaeljroeslein@gmail.com>
2019-09-21 01:24:41 -0400

    Added hover effect to Burger icon

--- src/components/Burger/styles.js
+++ src/components/Burger/styles.js
@@ -21,6 +21,15 @@ export const burgerStyles = css`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
+  padding: 1rem;
+  border: 1px solid var(--primary);
+  border-radius: 50%;
+  transition: background 250ms;
+
+  :hover {
+    border: 1px solid white;
+    background: white;
+  }
 `;
 
 export const pattyStyles = css`
