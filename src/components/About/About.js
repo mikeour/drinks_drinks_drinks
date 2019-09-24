f24c36ef8baffd5dc7a975ee0f8d40401846aca0
Michael Roeslein <michaeljroeslein@gmail.com>
2019-09-24 16:01:00 -0400

    Updated About component to include links to social media

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,9 @@
 import React from "react";
+import GithubLogo from "../../assets/github-logo.png";
+import LinkedinLogo from "../../assets/linkedin-logo.png";
 
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
+import { css, jsx } from "@emotion/core";
 import { aboutStyles } from "./styles";
 
 const About = () => (
@@ -18,6 +20,82 @@ const About = () => (
       Next time you're in the mood for a new recipe, I hope you think of this!
     </p>
     <p>Stay tuned for new features!</p>
+    <div
+      css={css`
+        display: flex;
+        align-items: center;
+      `}
+    >
+      <img
+        css={css`
+          display: inline-block;
+          width: 25px;
+          height: 25px;
+          margin: 0 1rem;
+        `}
+        src={GithubLogo}
+      ></img>
+      <a
+        href="https://www.github.com/mikeour"
+        rel="noopener noreferrer"
+        target="_blank"
+        css={css`
+          text-decoration: none;
+          color: black;
+
+          :visited {
+            color: black;
+          }
+
+          :hover {
+            font-weight: bold;
+          }
+        `}
+      >
+        <span css={css``}>GitHub</span>
+      </a>
+    </div>
+    <div
+      css={css`
+        display: flex;
+        align-items: center;
+      `}
+    >
+      <img
+        css={css`
+          display: inline-block;
+          width: 25px;
+          height: 25px;
+          margin: 0 1rem;
+        `}
+        src={LinkedinLogo}
+      ></img>
+      <a
+        href="https://www.linkedin.com/in/michaelroeslein"
+        rel="noopener noreferrer"
+        target="_blank"
+        css={css`
+          text-decoration: none;
+          color: black;
+
+          :visited {
+            color: black;
+          }
+
+          :hover {
+            font-weight: bold;
+          }
+        `}
+      >
+        <span
+          css={css`
+            display: inline-block;
+          `}
+        >
+          LinkedIn
+        </span>
+      </a>
+    </div>
   </div>
 );
 
